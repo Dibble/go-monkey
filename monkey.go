cc632fce2817486fb336b16e0abe3a79b30d1209
Dibble <officer.dibble1@gmail.com>
2016-09-14 11:33:22 +0100

    Extracted monkey to a function, run via a goroutine

--- monkey.go
+++ monkey.go
@@ -7,8 +7,8 @@ import (
 )
 
 const wordLength = 4
-const letterBytes = "abcdefghijklmnopqrstuvwxyz"
 
+const letterBytes = "abcdefghijklmnopqrstuvwxyz"
 func randString(length int) string {
   result := make([]byte, length)
 
@@ -19,27 +19,35 @@ func randString(length int) string {
   return string(result)
 }
 
+func monkey(word string, result chan<- int) {
+  guess := randString(len(word))
+  guesses := 1
+
+  for guess != word {
+    guess = randString(len(word))
+    guesses++
+  }
+
+  result <- guesses
+}
+
 func main() {
   rand.Seed(time.Now().UnixNano())
-  fmt.Println("Monkeys!")
 
   word := randString(wordLength)
   fmt.Printf("Word to guess: %s\n", word)
 
   startTime := time.Now().UnixNano()
-  guess := randString(wordLength)
-  guesses := 1
 
-  for guess != word {
-    guess = randString(wordLength)
-    guesses++
-  }
+  result := make(chan int)
+  go monkey(word, result)
+  guesses := <- result
+
   endTime := time.Now().UnixNano()
-
   totalTime := endTime - startTime
 
   fmt.Println("Guessed the word!")
-  fmt.Printf("In %d iterations.\n", guesses)
+  fmt.Printf("In %d guesses.\n", guesses)
   fmt.Printf("Total time: %dns\n", totalTime)
   fmt.Printf("%dns per guess.\n", totalTime / int64(guesses))
 }
