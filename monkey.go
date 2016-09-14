a357bf9f45021ecf35f29aa576398a98bd6fcec1
James Cotterell <james.cotterell@newvoicemedia.com>
2016-09-14 17:08:21 +0100

    Now running 4 monkeys as goroutines. This is super slow.

--- monkey.go
+++ monkey.go
@@ -19,35 +19,70 @@ func randString(length int) string {
   return string(result)
 }
 
-func monkey(word string, result chan<- int) {
-  guess := randString(len(word))
-  guesses := 1
+func printStats(guesses int, totalTime float64) {
+  fmt.Printf("In %d guesses.\n", guesses)
+  fmt.Printf("Total time: %.2f sseconds\n", totalTime / float64(1000000000))
+  fmt.Printf("%.0fns per guess.\n\n", totalTime / float64(guesses))
+}
 
-  for guess != word {
-    guess = randString(len(word))
-    guesses++
+func monkey(words <-chan string, result chan<- int) {
+  current_word := ""
+  guesses := 0
+
+  for true {
+    select {
+    case word := <- words:
+      guesses = 0
+      current_word = word
+    default:
+      if current_word != "" {
+        guess := randString(len(current_word))
+        guesses++
+
+        if guess == current_word {
+          result <- guesses
+          current_word = ""
+        }
+      }
+    }
   }
-
-  result <- guesses
 }
 
 func main() {
   rand.Seed(time.Now().UnixNano())
 
-  word := randString(wordLength)
-  fmt.Printf("Word to guess: %s\n", word)
+  words_channels := make([]chan string, 4)
+  result_channels := make([]chan int, 4)
+  for j := 0; j < 4; j++ {
+    words_channels[j] = make(chan string)
+    result_channels[j] = make(chan int)
 
-  startTime := time.Now().UnixNano()
+    go monkey(words_channels[j], result_channels[j])
+  }
 
-  result := make(chan int)
-  go monkey(word, result)
-  guesses := <- result
+  for i := 4; i <= 8; i++ {
+    word := randString(i)
+    fmt.Printf("Word to guess: %s\n", word)
 
-  endTime := time.Now().UnixNano()
-  totalTime := endTime - startTime
+    startTime := time.Now()
 
-  fmt.Println("Guessed the word!")
-  fmt.Printf("In %d guesses.\n", guesses)
-  fmt.Printf("Total time: %dns\n", totalTime)
-  fmt.Printf("%dns per guess.\n", totalTime / int64(guesses))
+    for _, word_channel := range words_channels {
+      word_channel <- word
+    }
+
+    select {
+    case guesses := <- result_channels[0]:
+      totalTime := time.Since(startTime)
+      printStats(guesses, float64(totalTime))
+    case guesses := <- result_channels[1]:
+      totalTime := time.Since(startTime)
+      printStats(guesses, float64(totalTime))
+    case guesses := <- result_channels[2]:
+      totalTime := time.Since(startTime)
+      printStats(guesses, float64(totalTime))
+    case guesses := <- result_channels[3]:
+      totalTime := time.Since(startTime)
+      printStats(guesses, float64(totalTime))
+    }
+  }
 }
